66b314b2d394f972b1a27e5cc22934cd042e63cd
Michael Nazaryan <72793688+MkhlNzrn@users.noreply.github.com>
2024-08-30 00:59:44 +0300

    Add files via upload

---
+++ src/pages/TestPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Header from '../components/start/Header'
+import Footer from '../components/start/Footer'
+
+import Test from '../components/test/Test'
+
+
+
+function ContactPage() {
+  return (
+    <>
+    <Header/>
+    <Test/>
+    <Footer/>
+    </>
+  )
+}
+
+export default ContactPage
